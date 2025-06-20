b1e3c6ce9f562a2e3874036ba52ca3773da33e88
SUJAL <130461887+sujal932538@users.noreply.github.com>
2025-06-20 22:36:05 +0530

    file_added

---
+++ first.js
@@ -0,0 +1,76 @@
+/*console.log("Welcome to DevSync.in");
+
+let fullname="sujal"
+let Surname="Titare"
+
+console.log(fullname+" "+ Surname);
+
+
+
+
+const product ={
+    title : "PEN",
+    rating : 2,
+    price : 10
+
+};
+
+console.log(product);
+document.log(product);
+*/
+
+// let pass= prompt("Enter the password");
+
+// if( pass === "4518"){
+
+//     alert("valid");
+// }else{
+    
+//     alert("INvalid");
+
+
+// }
+
+
+// let a=Math.random()
+// let user=prompt("Enter the number to win")
+
+
+//     while(user!=a){
+//         console.log("LOSE");
+//     }
+
+//     console.log("YOU WIN")
+
+
+
+    let a="tu ka karat hai idhar";
+
+    console.log(a.length);
+
+
+
+    let template=`IT is a special type of string`;
+
+
+    console.log(template);
+    console.log(typeof(template))
+
+
+    console.log(template.toUpperCase(0));
+    console.log(template.slice(0    ));
+
+
+    let array=[87,99,65,45,45,34,56]
+
+    console.log(array);
+
+    let sum=0;
+
+    for(let i=0; i<array.length;i++){
+        // sum=sum+array[i];
+        console.log(array(i))
+
+    }
+
+    // console.log(sum);
