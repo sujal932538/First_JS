ab3947dc27c8a6fad5b217ac0da7e00c2723c808
sujal932538 <sujaltitare1@gmail.com>
2025-06-21 20:27:42 +0530

    first commit

--- first.js
+++ first.js
@@ -1,3 +1,159 @@
+
+/*console.log("Welcome to DevSync.in");
+
+let fullname="sujal"
+let Surname="Titare"
+
+console.log(fullname+" "+ Surname);
+
+
+
+
+const product ={
+    title : "PEN",
+    rating : 2,
+    price : 10
+
+};
+
+console.log(product);
+document.log(product);
+*/
+
+// let pass= prompt("Enter the password");
+
+// if( pass === "4518"){
+
+//     alert("valid");
+// }else{
+    
+//     alert("INvalid");
+
+
+// }
+
+
+// let a=Math.random()
+// let user=prompt("Enter the number to win")
+
+
+//     while(user!=a){
+//         console.log("LOSE");
+//     }
+
+//     console.log("YOU WIN")
+
+
+
+    // let a="tu ka karat hai idhar";
+
+    // console.log(a.length);
+
+
+
+    // let template=`IT is a special type of string`;
+
+
+    // console.log(template);
+    // console.log(typeof(template))
+
+
+    // console.log(template.toUpperCase(0));
+    // console.log(template.slice(0    ));
+
+
+    // let array=[87,99,65,45,45,34,56]
+
+    // console.log(array);
+
+    // let sum=0;
+
+    // for(let i=0; i<array.length;i++){
+    //     // sum=sum+array[i];
+    //     console.log(array(i))
+
+    // }
+
+    // console.log(sum);
+
+
+/*console.log("Welcome to DevSync.in");
+
+let fullname="sujal"
+let Surname="Titare"
+
+console.log(fullname+" "+ Surname);
+
+
+
+
+const product ={
+    title : "PEN",
+    rating : 2,
+    price : 10
+
+};
+
+console.log(product);
+document.log(product);
+*/
+
+// let pass= prompt("Enter the password");
+
+// if( pass === "4518"){
+
+//     alert("valid");
+// }else{
+    
+//     alert("INvalid");
+
+
+// }
+
+
+// let a=Math.random()
+// let user=prompt("Enter the number to win")
+
+
+//     while(user!=a){
+//         console.log("LOSE");
+//     }
+
+//     console.log("YOU WIN")
+
+
+
+    // let a="tu ka karat hai idhar";
+
+    // console.log(a.length);
+
+
+
+    // let template=`IT is a special type of string`;
+
+
+    // console.log(template);
+    // console.log(typeof(template))
+
+
+    // console.log(template.toUpperCase(0));
+    // console.log(template.slice(0    ));
+
+
+    // let array=[87,99,65,45,45,34,56]
+
+    // console.log(array);
+
+    // let sum=0;
+
+    // for(let i=0; i<array.length;i++){
+    //     // sum=sum+array[i];
+    //     console.log(array(i))
+
+    // }
+
+    // console.log(sum);
+
 /*console.log("Welcome to DevSync.in");
 
 let fullname="sujal"
@@ -65,12 +221,35 @@ document.log(product);
 
     console.log(array);
 
-    let sum=0;
+    // let sum=0;
 
-    for(let i=0; i<array.length;i++){
-        // sum=sum+array[i];
-        console.log(array(i))
+    // for(let i=0; i<array.length;i++){
+    //     // sum=sum+array[i];
+    //     console.log(array(i))
 
-    }
+    // }
 
     // console.log(sum);
+
+    
+let numbers = [100, 200, 150, 300, 250];
+
+
+let sum = 0;
+for (let i = 0; i < numbers.length; i++) {
+  sum += numbers[i];
+}
+
+let average = sum / numbers.length;
+console.log("Average of numbers:", average);
+
+
+console.log("10% offer on each number:");
+for (let i = 0; i < numbers.length; i++) {
+  let offer = numbers[i] * 0.10;
+  console.log(`10% of ${numbers[i]} = ${offer}`);
+}
+
+
+
+
